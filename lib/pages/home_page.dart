212d7ab62c81e1562f0b3cf4886ba062911f9880
Potato1Peeler <alexku05100@gmail.com>
2025-04-03 12:54:52 -0600

    CartAdd

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -86,10 +86,38 @@ class _HomePageState extends State<HomePage> {
                 SmoothieTab(),
                 PancakesTab(),
                 PizzaTab(),
-              
               ]),
-            )
+            ),
             //4. Carrito (Cart)
+            Container(
+              color: Colors.white,
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(left: 28),
+                    child: Column(
+                      children: [
+                        Text('2 items | \$45 ',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            )),
+                        Text('Delivery Charges Included')
+                      ],
+                    ),
+                  ),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.pink,
+                        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                      ),
+                      onPressed: () {}, 
+                      child: const Text('View Cart'))
+                ],
+              ),
+            )
           ],
         ),
       ),
